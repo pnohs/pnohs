cffc36cb90f0105fd6741a91f6634473c30bebd4
genshen <genshenchu@gmail.com>
2018-09-12 09:29:31 +0800

    feat(output): add result conversion from map-vector to 1-d array.

--- src/utils/sim_reduction.hpp
+++ src/utils/sim_reduction.hpp
@@ -14,10 +14,12 @@
  * In this class, it process simulation result like storing simulation data, filter data.
  * @tparam T the simulation result type for one step on one node.
  */
-template<class T>
+template<typename T>
 class SimReduction {
 public:
     static const unsigned int BUFFER_FLAG_NONE = 0;
+    typedef std::map<_type_node_id, std::vector<T>> simred;
+    typedef typename simred::iterator simred_iter;
 
     /**
      * We store simulation data in a buffered map-vector @var r.
@@ -36,11 +38,42 @@ public:
      * For each node, this function will be called to save to @var r simulation data in each time-step.
      * @note: you can override @fn filter to filter simulation data.
      *
-     * @param context model context for current simulation node.
+     * @param res the simulation data for simulation node specified by node id @param id.
      * @param id the node id.
      * @param time_step current simulation time step.
      */
-    void OnOneStep(T &res, const _type_node_id id, const unsigned long time_step);
+    void OnOneStep(const _type_node_id id, const unsigned long time_step, T &res);
+
+    /**
+     * this method convert simulation results to 2-d array(in fact, it is 1-d due to the consequent memory) specified by @param flows.
+     * it contains {nodes count}s rows, in each row, it contains {time_steps}s col,
+     * representing {time_steps}s flow data in each time step on this simulation node.
+     * @param result_2d the 1-d array to store the flat 2d array.
+     * @param len the length of 1d array
+     * @param steps the total simulation steps.
+     * @return return true if convert success, return false for otherwise.
+     */
+    bool flat(T *result_2d, const unsigned long len, const unsigned long steps);
+
+    /**
+     * This method set the array @var result_vec for usage.
+     * It copy memory of the vector form map @param r whose key is @param id to array @param result_vec.
+     * If the key-value is not found in map @param r, 
+     * this function will be returned leaving array @param result_vec untouched.
+     * @param result_vec receive array to save result data form map-vector.
+     * @param len the length of array @param result_vec
+     * @return true for set array @param result_vec successfully, false for otherwise.
+     */
+    bool flatNode(T *result_vec, const _type_node_id id, const unsigned long len);
+
+protected:
+    /*
+     * simulation results.
+     * it is a map, the key of this map is the simulation node's id;
+     * and its value is a vector, ths vector length equals to simulation time-steps;
+     * items in vector represents the flow of river outlet in each time-step.
+     */
+    simred r;
 
     /**
      * simulation data filter.
@@ -50,15 +83,6 @@ public:
      * @return false fot skipping processing data, true for processing data.
      */
     virtual bool filter(const _type_node_id id, const unsigned long time_step);
-
-protected:
-    /*
-     * simulation results.
-     * it is a map, the key of this map is the simulation node's id;
-     * and its value is a vector, ths vector length equals to simulation time-steps;
-     * items in vector represents the flow of river outlet in each time-step.
-     */
-    std::map<_type_node_id, std::vector<T>> r;
 
 private:
     const unsigned long buffer_size;
@@ -73,7 +97,7 @@ SimReduction<T>::~SimReduction() {
 }
 
 template<class T>
-void SimReduction<T>::OnOneStep(T &res, const _type_node_id id, const unsigned long time_step) {
+void SimReduction<T>::OnOneStep(const _type_node_id id, const unsigned long time_step, T &res) {
     if (!filter(id, time_step)) {
         return;
     }
@@ -90,6 +114,51 @@ template<class T>
 bool SimReduction<T>::filter(const _type_node_id id, const unsigned long time_step) {
     // return false as default value, which means it does not process any simulation data.
     return false;
+}
+
+template<class T>
+bool SimReduction<T>::flat(T *result_2d, const unsigned long len, const unsigned long steps) {
+    // test length of map/vector data.
+    simred_iter iter_len_test;
+    int len_real = 0;
+    for (iter_len_test = r.begin(); iter_len_test != r.end(); iter_len_test++) {
+        len_real += iter_len_test->second.size();
+    }
+    if (len_real != len || len_real != r.size() * steps) {
+        return false;
+    }
+
+    // allocate memory for 2d indexing.
+    unsigned long x = r.size();
+    unsigned long y = steps;
+    T **flows2d = new T *[x];
+    *flows2d = result_2d;
+    for (int i = 0; i < x; i++) {
+        flows2d[i] = result_2d + i * y;
+    }
+    // copy values
+    int i = 0;
+    simred_iter iter;
+    for (iter = r.begin(); iter != r.end(); iter++) {
+        int j = 0;
+        for (T _flow :iter->second) {
+            flows2d[i][j] = _flow;
+            j++;
+        }
+        i++;
+    }
+    delete[]flows2d;
+    return true;
+}
+
+template<typename T>
+bool SimReduction<T>::flatNode(T *result_vec, const _type_node_id id, const unsigned long len) {
+    std::vector<T> &temp = r[id];
+    if (temp.size() != len) {
+        return false;
+    }
+    std::copy(temp.begin(), temp.begin() + len, result_vec);
+    return true;
 }
 
 #endif //PNOHS_SIM_REDUCTION_H
--- test/unit/utils/sim_reduction_test.cpp
+++ test/unit/utils/sim_reduction_test.cpp
@@ -16,13 +16,26 @@ public:
     FRIEND_TEST(sim_reduction_test_filter, sim_reduction_test);
 };
 
+class SimReductionTest2 : public SimReduction<int> {
+public:
+    SimReductionTest2() : SimReduction(SimReductionTest::BUFFER_FLAG_NONE) {} // no buffer
+
+    bool filter(const _type_node_id id, const unsigned long time_step) override {
+        return true;
+    }
+
+    FRIEND_TEST(sim_reduction_test_flat, sim_reduction_test);
+
+    FRIEND_TEST(sim_reduction_test_flat_node, sim_reduction_test);
+};
+
 TEST(sim_reduction_test_filter, sim_reduction_test) {
     SimReductionTest simTest;
     const unsigned int ids = 10, ts = 30;
     for (unsigned int id = 0; id < ids; id++) {
         for (unsigned int t = 0; t < ts; t++) {
             int res = id * 10 + t;
-            simTest.OnOneStep(res, id, t);
+            simTest.OnOneStep(id, t, res);
         }
     }
 
@@ -34,4 +47,48 @@ TEST(sim_reduction_test_filter, sim_reduction_test) {
             t += 2; // select all filtered-passed t.
         }
     }
+}
+
+TEST(sim_reduction_test_flat, sim_reduction_test) {
+    SimReductionTest2 simTest;
+    const unsigned int ids = 10, ts = 30;
+    for (unsigned int id = 0; id < ids; id++) {
+        for (unsigned int t = 0; t < ts; t++) {
+            int res = id * 10 + t;
+            simTest.OnOneStep(id, t, res);
+        }
+    }
+    // now, map size will be 10 and each element in map is a vector with size 30.
+    int *flows = new int[ids * ts];
+    simTest.flat(flows, ids * ts, ts);
+    // test
+    int i = 0;
+    for (unsigned int id = 0; id < ids; id++) {
+        for (unsigned int t = 0; t < ts; t++) {
+            int res = id * 10 + t; // the same as above
+            EXPECT_EQ(flows[i], res);
+            i++;
+        }
+    }
+    delete flows;
+}
+
+TEST(sim_reduction_test_flat_node, sim_reduction_test) {
+    SimReductionTest2 simTest;
+    const unsigned int ids = 10, ts = 30;
+    for (unsigned int id = 0; id < ids; id++) {
+        for (unsigned int t = 0; t < ts; t++) {
+            int res = id * 10 + t;
+            simTest.OnOneStep(id, t, res);
+        }
+    }
+    int a[ts];
+    const unsigned int &data_len = ts;
+    EXPECT_EQ(simTest.flatNode(a, 11, data_len), false); // id not exists
+    EXPECT_EQ(simTest.flatNode(a, 2, data_len), true); // id = 2 exists
+    for (int i = 0; i < data_len; i++) {
+        EXPECT_EQ(a[i], 2 * 10 + i); // id = 2
+    }
+    EXPECT_EQ(simTest.flatNode(a, 2, data_len + 1), false); // len not match
+    EXPECT_EQ(simTest.flatNode(a, 11, data_len + 1), false); // id not exists  & len not match
 }
