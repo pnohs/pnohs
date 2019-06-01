f92c21de1c7dd4f7aaf4c3597b98673878beedfe
genshen <genshenchu@gmail.com>
2019-06-01 15:22:46 +0800

    test(params): add more tests for params binding in params list data structure.
    data binging: binding params array to c++ aliens.

--- test/unit/ds/param_list_test.cpp
+++ test/unit/ds/param_list_test.cpp
@@ -12,5 +12,18 @@ class TestParamsList : public ParamsList<3> {
 
 TEST(params_list_test_get_count, params_list_test) {
     TestParamsList list;
-    EXPECT_DOUBLE_EQ(list.getParamsSize(), 3);
+    EXPECT_EQ(list.getParamsSize(), 3);
+}
+
+// test params binding in class statically.
+class TestParamsList2 : public ParamsList<3> {
+public:
+    param_const &P1 = data[0];
+};
+
+TEST(params_list_test_params_name, params_list_test) {
+    TestParamsList2 list;
+    list.P1 = 2;
+    EXPECT_DOUBLE_EQ(list.P1, 2);
+    EXPECT_DOUBLE_EQ(list.data[0], 2);
 }
