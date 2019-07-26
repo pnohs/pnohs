e1120f10ebb031b5dc31360861e6296abbd8a437
genshen <genshenchu@gmail.com>
2019-07-26 15:26:27 +0800

    feat(scheduler): add blocking time counting for simulation nodes selecting.
    description: thread will go to blocking status when waiting for available simulation nodes.

--- src/sys_context.h
+++ src/sys_context.h
@@ -26,13 +26,37 @@ public:
      */
     void abort(const std::string &reason, int code);
 
+    inline double getBlockingTime() {
+        return blocking_time;
+    }
+
+    inline unsigned long getBlockTimes() {
+        return block_times;
+    }
+
+    /**
+     * call this function to accumulate blocking time and blocking count.
+     * @param delta_blocking_time the blocking time in one thread blocking.
+     */
+    void onThreadBlock(double delta_blocking_time);
+
     // pthread variable
     int _t_waiting = 0; // todo add comments here.
     pthread_mutex_t _t_mu;
     pthread_cond_t _t_cond;
 //    kiwi::mpi_process mpi_sim_process; // todo move here
+
 private:
-//    const ConfigToml *pConfig;
+    /**
+     * the cumulative time of blocking pthread waiting for available simulation nodes.
+     */
+    double blocking_time = 0.0;
+
+    /**
+     * the count of blocking pthread waiting for available simulation nodes.
+     * It will plus 1 if selecting simulation node goes to blocked.
+     */
+    unsigned long block_times = 0;
 
 };
 
