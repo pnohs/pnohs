f60223359392780e85f7892ccc3d34e7921d7050
genshen <genshenchu@gmail.com>
2020-06-12 16:47:30 +0800

    feat: add feature of setting simulation communicator domain

--- src/utils/sim_domain.cpp
+++ src/utils/sim_domain.cpp
@@ -5,3 +5,9 @@
 #include "sim_domain.h"
 
 kiwi::mpi_process domain::mpi_sim_process;
+
+void domain::setCommDomain(MPI_Comm comm) {
+    mpi_sim_process.comm = comm;
+    MPI_Comm_rank(comm, &mpi_sim_process.own_rank);
+    MPI_Comm_size(comm, &mpi_sim_process.all_ranks);
+}
