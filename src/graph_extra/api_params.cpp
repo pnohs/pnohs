bdcb43fe46c514d70077f900c79bf330fbc5ef9f
genshen <genshenchu@gmail.com>
2021-06-02 14:56:48 +0800

    feat(params): pass "local params" (not global parames) to function ApiParams::setNodesParams

--- src/graph_extra/api_params.cpp
+++ src/graph_extra/api_params.cpp
@@ -24,17 +24,10 @@ ApiParams::~ApiParams() {
     GraphMetaBuilder::destroyMetaEntity(graph_meta); // graph in meta will also be deleted here.
 }
 
-void ApiParams::setNodesParams(SContext *s_ctx, _type_param params[], const size_t length, const size_t size) {
-    _type_param *_local_params = new _type_param[size * graph_meta.local_nodes_count]; // fixme
+void ApiParams::setNodesParams(SContext *s_ctx, _type_param _local_params[], const size_t length) {
     // runoff parameters vector and routing parameter vector points to array _local_params.
     std::vector<_type_param *> runoff_params(graph_meta.local_nodes_count);
     std::vector<_type_param *> routing_params(graph_meta.local_nodes_count);
-
-    if (!distParams(params, length, size, _local_params, graph_meta.g_nodes_counts, root)) {
-        kiwi::logs::e("params", "error parameters size.\n");
-        delete[]_local_params;
-        return;
-    }
 
     // set parameters vector.
     for (int i = 0; i < graph_meta.local_nodes_count; i++) {
@@ -46,13 +39,14 @@ void ApiParams::setNodesParams(SContext *s_ctx, _type_param params[], const size
             routing_params[i] = _local_params;
             continue;
         }
-        runoff_params[i] = _local_params + i * size;
-        routing_params[i] = _local_params + node->_p_runoff_model->paramCount();
+        runoff_params[i] = _local_params;
+        _local_params += node->_p_runoff_model->paramCount();
+        routing_params[i] = _local_params;
+        _local_params += node->_p_routing_model->paramCount();
     }
 
     s_ctx->pNodesPool->setNodesParams(*(graph_meta.local_nodes_id_lists), runoff_params,
                                       routing_params); // todo data copy
-    delete[]_local_params;
 }
 
 bool ApiParams::distParams(_type_param params[], const size_t length, const size_t size,
